1b452b2365ff9bff3057b7899ce83f1ed0619051
hmon <wtthumon@gmail.com>
2025-07-11 17:29:11 +0200

    refactored

--- maximise_gap_3440.py
+++ maximise_gap_3440.py
@@ -2,32 +2,27 @@
 class Solution:
     def maxFreeTime(self, eventTime, startTime, endTime):
         top_three_gaps = self.make_top_three(eventTime, startTime, endTime)
-        
-        biggest = -1
-        space = 0
-        for ind, i in enumerate(startTime[:-1]):
-            gap_index = 0
-            while gap_index < 3:
-                event_length = endTime[ind] - startTime[ind]
-                if ind == 0:
-                    end = 0
-                else:
-                    end = endTime[ind-1]
-                continous_space = startTime[ind+1] - end
+
+        biggest = top_three_gaps[0][0]
+        for ind in range(len(startTime[:-1])):
+            if ind == 0:
+                end = 0
+            else:
+                end = endTime[ind-1]
+            event_length = endTime[ind] - startTime[ind]
+            continous_space = startTime[ind+1] - end
+            biggest = max(biggest, continous_space - event_length)
+            for gap_index in range(3):
                 if top_three_gaps[gap_index][1] != ind + 1 \
                     and top_three_gaps[gap_index][1] != ind \
                     and event_length <= top_three_gaps[gap_index][0]:
-                    space = continous_space
-                else:
-                    space = continous_space - event_length
-                if space > biggest:
-                    biggest = space
-                gap_index+=1
-  
+                    biggest = max(biggest, continous_space)
+                    break
+
         return biggest
     
     def make_top_three(self,eventTime, startTime, endTime):
-        top_three_gaps = []
+        top_three_gaps = [(-1, -1), (-1, -1), (-1, -1)]
         startTime.append(eventTime)
         endTime.append(eventTime)
         for i in range(len(startTime)):
@@ -36,10 +31,16 @@ class Solution:
             else:
                 gap = startTime[i]-endTime[i-1]
                 
-            top_three_gaps.append([gap,i])
-        top_three_gaps = sorted(top_three_gaps, key=lambda x: x[0],reverse=True)
-        if len(top_three_gaps)>3:
-            top_three_gaps = top_three_gaps[:3]
+            for place in range(2, -1, -1):
+                if gap > top_three_gaps[place][0]:
+                    top_three_gaps[place] = (gap, i)
+                    break
+            for place in range(2, 0, -1):
+                if top_three_gaps[place-1][0] < top_three_gaps[place][0]:
+                    tmp = top_three_gaps[place]
+                    top_three_gaps[place] = top_three_gaps[place-1]
+                    top_three_gaps[place - 1] = tmp
+
         return top_three_gaps
                     
 
@@ -47,4 +48,5 @@ class Solution:
 sol = Solution()
 #sol.maxFreeTime(5,[1,3],[2,5])
 #sol.maxFreeTime(10, [0,7,9], [1,8,10])
-sol.maxFreeTime(10,[0,3,7,9], [1,4,8,10])
+tmp = sol.maxFreeTime(10,[0,3,7,9], [1,4,8,10])
+print(tmp)
