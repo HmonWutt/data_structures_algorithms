f61a4970101736b88c5cccac94cd649f7c2e0089
hmon <wtthumon@gmail.com>
2025-07-13 02:14:58 +0200

    not working yet

--- move_events_to_maximise_free_time.py
+++ move_events_to_maximise_free_time.py
@@ -1,47 +1,38 @@
+from collections import deque 
 def free_up_time(eventTime, k,startTime,endTime,):
     count = len(startTime)
-    gaps = []
-    maxFree = 0
-    
-
-    for i in range(0,count):
-        if i == 0:
+    ind = 0
+    dq = deque([])
+    while ind < count:
+        if ind == 0:
             window_start = 0
         else:
-            window_start = endTime[i-1]
+            window_start = endTime[ind-1]
 
-        if i == count -1:
+        if ind == count -1:
             window_end = eventTime
         else:
-            try:
-                window_end = startTime[i+1] 
-            
-            except IndexError:
-                window_end = eventTime
-        
-        if window_start == startTime[i] or window_end == endTime[i]:
-            moved = 0
+            window_end = startTime[ind+1] 
+        event_length = endTime[ind] - startTime[ind]
+        ind+=1
+        gap_1 = window_start - startTime[ind]
+        gap_ = window_end - window_start - event_length
+        if dq:
+            to_add = dq[-1]
         else:
-            moved = 1
-        event_length = endTime[i] - startTime[i]
-        gap = window_end - window_start - event_length
-        gaps.append([gap,moved])
-    total = 0
-    print("len: ",len(gaps),gaps)
-    for i in range(len(gaps)-1):
-        for j in range(i,i+k):
-            print(j)
+            to_add = 0
+        dq.append(gap+to_add) 
+        if len(dq) > k :
+            to_remove = dq.popleft()
+            dq[-1] -= to_remove
+      
+    print(dq)
+    return  max(dq)
         
-            total  += (gaps[j][0] *gaps[j][1])
-        maxFree = max(maxFree,total)
-        total = 0
-        
-    return maxFree
-
     
-
-
-
-print(free_up_time(34,2,[0,17],[14,19]))
-
-    
+#print(free_up_time(64,2,[29,49],[37,54]))
+#print(free_up_time(10,1,[0,2,9],[1,4,10]))
+#print(free_up_time(21,1,[7,10,16],[10,14,18]))
+#print(free_up_time(30,2,[1,15,17],[14,17,29]))
+#print(free_up_time(5,1,[1,3],[2,5]))
+print(free_up_time(21,1,[7,10,16],[10,14,18]))
