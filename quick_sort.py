c3ecba1f42fe5cdb511d898ecef839423d6c6f9d
hmon <wtthumon@gmail.com>
2025-07-23 12:31:19 +0200

    quicksort practice

--- quick_sort.py
+++ quick_sort.py
@@ -1,26 +1,20 @@
-def qs(arr,s,e):
-    if s >= e:
+def quick_sort(arr,s,e):
+    left = s
+    right = e
+    pivot = s+(e-s)//2
+    if s == e:
         return arr
-    l= s
-    r= e
-    p = s+(e-s)//2
-    while l < r:
-        while arr[l]<arr[p]:
-            l+=1
+    while left<=right:
+        while arr[left]<arr[pivot]:
+            left +=1
+        while arr[right]>arr[pivot]:
+            right-=1
 
-        while arr[r]> arr[p]:
-            r-=1
-
-        if l< r:
-            arr[r],arr[l] = arr[l],arr[r]
-            l+=1
-            r-=1
-    qs(arr,s,p)
-    qs(arr,p+1,e)
-
-
-#arr = [1,10,9,4,3,2]
-#arr = [5,4,3,2,1]
-arr = [3,2]
-print(qs(arr,0,len(arr)-1)  )
+        if left <=right:
+            arr[left],arr[right] =arr[right],arr[left]
+            left+=1
+    quick_sort(arr,s,pivot)
+    quick_sort(arr,pivot+1,e)
+arr = [8,3,8,9,2,4,6,7,1,2]
+print(quick_sort(arr,0,len(arr)-1))
 print(arr)
