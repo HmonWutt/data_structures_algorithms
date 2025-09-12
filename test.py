7ad6fd38edade53954e8a4bb978f65447c6e48ad
HmonWutt <wtthumon@gmail.com>
2025-09-12 12:31:59 +0200

    cleaned up

--- test.py
+++ test.py
@@ -1,23 +1,16 @@
-
-from pydoc import doc
-
-
 def find_longest_word(document, longest_word=""):
-    current_longest = longest_word
     new_longest= ""
-    if len(document.split(maxsplit=1)) <2:
-        if len(document)-1 > len(longest_word):
-            return document[:-1]
-        return longest_word
-        
+    if document.strip() == "":
+        return ""
+    
     words = document.split(maxsplit=1)
-    if len(words[0])> len(current_longest):
-        new_longest = find_longest_word(words[1],words[0])
-    else:
-        new_longest = find_longest_word(words[1],current_longest)
-    if len(current_longest)<len(new_longest):
-        current_longest = new_longest
-    return current_longest
+    if len(words) <1:
+        return longest_word
+    if len(words[0])> len(longest_word):
+        longest_word= words[0]
+    if len(words)<2:
+        return longest_word
+    return find_longest_word(words[1],longest_word)
 
-print(find_longest_word("Either that wallpaper goes, or I do.", ""))
 
+print(find_longest_word("I am happy",""))
