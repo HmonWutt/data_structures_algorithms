5c5e9d001df9b54391597058280776325b4aa656
HmonWutt <wtthumon@gmail.com>
2025-09-12 12:24:45 +0200

    find longest word recursively first iteration

--- test.py
+++ test.py
@@ -1,14 +1,23 @@
-def combination(string,mod):
-    total = []
-    if not string:
-        return [mod]
-    ch = string[0]
-    total+=combination(string[1:],mod+ch)
-    total+=combination(string[1:],mod)
-    return total
 
-    
+from pydoc import doc
 
-   
 
-print(combination("abc",""))
+def find_longest_word(document, longest_word=""):
+    current_longest = longest_word
+    new_longest= ""
+    if len(document.split(maxsplit=1)) <2:
+        if len(document)-1 > len(longest_word):
+            return document[:-1]
+        return longest_word
+        
+    words = document.split(maxsplit=1)
+    if len(words[0])> len(current_longest):
+        new_longest = find_longest_word(words[1],words[0])
+    else:
+        new_longest = find_longest_word(words[1],current_longest)
+    if len(current_longest)<len(new_longest):
+        current_longest = new_longest
+    return current_longest
+
+print(find_longest_word("Either that wallpaper goes, or I do.", ""))
+
